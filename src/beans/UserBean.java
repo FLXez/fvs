14340bfadac33a2fec13f058adad507d9fd9cc7a
FLXez <37533413+Swoodox@users.noreply.github.com>
2020-03-17 15:46:44 +0100

    Irgendwie läuft das nun

--- src/beans/UserBean.java
+++ src/beans/UserBean.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import ejb.UserEJB;
 import entities.User;
 
+@Named("userBean")
 @ApplicationScoped
 @ManagedBean
 public class UserBean {
@@ -23,6 +25,10 @@ public class UserBean {
 	public void add() {
 		User user = new User();
 		user.setEmail("YoutubeTest");
+		user.setPasswort("LEL");
+		user.setVorname("vorname");
+		user.setName("name");
+		user.setPrivilegien("Mitarbeiter");
 		userEJB.userSpeichern(user);
 		
 	}
