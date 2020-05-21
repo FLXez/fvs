5a81d2b5dbb7593b4f8717f142aaf12cdffbbac6
FLXez <37533413+Swoodox@users.noreply.github.com>
2020-05-21 19:05:46 +0200

    dbfiles new

--- dbfiles/fvs.sql
+++ dbfiles/fvs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 17. Mai 2020 um 13:57
+-- Erstellungszeit: 21. Mai 2020 um 19:05
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 7.1.1
 
@@ -44,7 +44,19 @@ INSERT INTO `buslinie` (`bid`, `nummer`, `Richtung`) VALUES
 (5, 41, 'H'),
 (6, 41, 'R'),
 (7, 55, 'H'),
-(8, 55, 'R');
+(8, 55, 'R'),
+(9, 20, 'H'),
+(10, 20, 'R'),
+(11, 666, 'H'),
+(12, 666, 'R'),
+(13, 25, 'H'),
+(14, 25, 'R'),
+(15, 15, 'H'),
+(16, 15, 'R'),
+(17, 11, 'H'),
+(18, 11, 'R'),
+(19, 56, 'H'),
+(20, 56, 'R');
 
 -- --------------------------------------------------------
 
@@ -57,8 +69,22 @@ CREATE TABLE `fahrt` (
   `bid` int(11) NOT NULL,
   `hidS` int(11) NOT NULL,
   `hidE` int(11) NOT NULL,
-  `uhrzeit` time NOT NULL
+  `uhrzeit` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `fahrt`
+--
+
+INSERT INTO `fahrt` (`fid`, `bid`, `hidS`, `hidE`, `uhrzeit`) VALUES
+(11, 1, 7, 5, '12:20'),
+(12, 2, 5, 7, '15:30'),
+(13, 1, 4, 9, '15:30'),
+(14, 1, 7, 6, '10:10'),
+(15, 1, 7, 9, '12:20'),
+(16, 1, 7, 9, '13:00'),
+(17, 1, 6, 2, '10:10'),
+(18, 11, 10, 3, '15:30');
 
 -- --------------------------------------------------------
 
@@ -82,7 +108,11 @@ INSERT INTO `haltestelle` (`hid`, `bezeichnung`) VALUES
 (4, 'tbahnhof'),
 (5, 'test'),
 (6, 'Irgendwo'),
-(7, 'Ich brauch noch eine für fix');
+(7, 'Ich brauch noch eine für fix'),
+(8, 'Julian'),
+(9, 'tester'),
+(10, 'test12'),
+(11, 'test34');
 
 -- --------------------------------------------------------
 
@@ -103,19 +133,21 @@ CREATE TABLE `linienabfolge` (
 --
 
 INSERT INTO `linienabfolge` (`lid`, `bidh`, `bidr`, `vid`, `position`) VALUES
-(34, 1, 2, 74, 0),
-(35, 1, 2, 75, 1),
-(36, 1, 2, 76, -1),
-(37, 3, 4, 77, 0),
-(38, 3, 4, 78, 1),
-(39, 3, 4, 79, 2),
-(40, 3, 4, 80, 3),
-(41, 5, 6, 77, 0),
-(42, 5, 6, 81, -1),
-(43, 5, 6, 82, -2),
-(44, 5, 6, 83, -3),
-(45, 5, 6, 84, -4),
-(46, 5, 6, 85, -5);
+(56, 1, 2, 92, 0),
+(57, 1, 2, 93, 1),
+(58, 1, 2, 94, 2),
+(59, 1, 2, 95, 3),
+(60, 1, 2, 96, -1),
+(61, 1, 2, 97, -2),
+(62, 1, 2, 98, 4),
+(63, 1, 2, 99, 5),
+(65, 5, 6, 104, 0),
+(66, 5, 6, 105, 1),
+(67, 5, 6, 106, 2),
+(68, 5, 6, 107, 3),
+(69, 11, 12, 108, 0),
+(70, 11, 12, 109, 1),
+(71, 11, 12, 93, 2);
 
 -- --------------------------------------------------------
 
@@ -159,18 +191,23 @@ CREATE TABLE `verbindung` (
 --
 
 INSERT INTO `verbindung` (`vid`, `hidS`, `hidE`, `dauer`) VALUES
-(74, 1, 5, 8),
-(75, 5, 7, 7),
-(76, 6, 1, 10),
-(77, 1, 4, 5),
-(78, 4, 2, 5),
-(79, 2, 7, 5),
-(80, 7, 6, 5),
-(81, 3, 1, 5),
-(82, 4, 3, 5),
-(83, 6, 4, 5),
-(84, 4, 6, 5),
-(85, 7, 4, 5);
+(92, 1, 2, 2),
+(93, 2, 3, 9),
+(94, 3, 4, 5),
+(95, 4, 5, 30),
+(96, 6, 1, 3),
+(97, 7, 6, 3),
+(98, 5, 8, 11),
+(99, 8, 9, 5),
+(100, 5, 2, 3),
+(101, 3, 2, 8),
+(102, 2, 1, 0),
+(104, 1, 6, 0),
+(105, 6, 2, 0),
+(106, 2, 8, 0),
+(107, 8, 5, 0),
+(108, 10, 11, 0),
+(109, 11, 2, 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -229,22 +266,22 @@ ALTER TABLE `verbindung`
 -- AUTO_INCREMENT für Tabelle `buslinie`
 --
 ALTER TABLE `buslinie`
-  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT für Tabelle `fahrt`
 --
 ALTER TABLE `fahrt`
-  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT für Tabelle `haltestelle`
 --
 ALTER TABLE `haltestelle`
-  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT für Tabelle `linienabfolge`
 --
 ALTER TABLE `linienabfolge`
-  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
@@ -254,7 +291,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `verbindung`
 --
 ALTER TABLE `verbindung`
-  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 --
 -- Constraints der exportierten Tabellen
 --
