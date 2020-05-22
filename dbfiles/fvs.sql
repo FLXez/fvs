6e2e22c67004f0cda546eff4129be72655675316
FLXez <37533413+Swoodox@users.noreply.github.com>
2020-05-22 13:38:15 +0200

    DB mit "guten" Daten

--- dbfiles/fvs.sql
+++ dbfiles/fvs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Mai 2020 um 19:05
+-- Erstellungszeit: 22. Mai 2020 um 13:37
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 7.1.1
 
@@ -37,26 +37,22 @@ CREATE TABLE `buslinie` (
 --
 
 INSERT INTO `buslinie` (`bid`, `nummer`, `Richtung`) VALUES
-(1, 12, 'H'),
-(2, 12, 'R'),
-(3, 14, 'H'),
-(4, 14, 'R'),
-(5, 41, 'H'),
-(6, 41, 'R'),
-(7, 55, 'H'),
-(8, 55, 'R'),
-(9, 20, 'H'),
-(10, 20, 'R'),
-(11, 666, 'H'),
-(12, 666, 'R'),
-(13, 25, 'H'),
-(14, 25, 'R'),
-(15, 15, 'H'),
-(16, 15, 'R'),
-(17, 11, 'H'),
-(18, 11, 'R'),
-(19, 56, 'H'),
-(20, 56, 'R');
+(1, 50, 'H'),
+(2, 50, 'R'),
+(3, 20, 'H'),
+(4, 20, 'R'),
+(5, 80, 'H'),
+(6, 80, 'R'),
+(7, 100, 'H'),
+(8, 100, 'R'),
+(9, 64, 'H'),
+(10, 64, 'R'),
+(11, 72, 'H'),
+(12, 72, 'R'),
+(13, 90, 'H'),
+(14, 90, 'R'),
+(15, 30, 'H'),
+(16, 30, 'R');
 
 -- --------------------------------------------------------
 
@@ -77,14 +73,25 @@ CREATE TABLE `fahrt` (
 --
 
 INSERT INTO `fahrt` (`fid`, `bid`, `hidS`, `hidE`, `uhrzeit`) VALUES
-(11, 1, 7, 5, '12:20'),
-(12, 2, 5, 7, '15:30'),
-(13, 1, 4, 9, '15:30'),
-(14, 1, 7, 6, '10:10'),
-(15, 1, 7, 9, '12:20'),
-(16, 1, 7, 9, '13:00'),
-(17, 1, 6, 2, '10:10'),
-(18, 11, 10, 3, '15:30');
+(1, 1, 1, 10, '12:20'),
+(2, 1, 2, 8, '10:10'),
+(3, 1, 8, 10, '15:30'),
+(4, 2, 10, 1, '15:30'),
+(5, 2, 7, 3, '12:20'),
+(6, 3, 6, 7, '07:00'),
+(7, 3, 4, 6, '12:00'),
+(8, 3, 3, 7, '10:10'),
+(9, 4, 7, 4, '18:20'),
+(10, 4, 7, 3, '13:00'),
+(11, 4, 6, 4, '12:10'),
+(12, 4, 5, 4, '11:11'),
+(13, 5, 10, 3, '16:44'),
+(14, 5, 10, 7, '12:21'),
+(15, 5, 9, 3, '14:59'),
+(16, 6, 3, 1, '12:30'),
+(17, 6, 3, 7, '14:10'),
+(18, 6, 2, 1, '23:50'),
+(19, 6, 7, 2, '12:10');
 
 -- --------------------------------------------------------
 
@@ -102,17 +109,16 @@ CREATE TABLE `haltestelle` (
 --
 
 INSERT INTO `haltestelle` (`hid`, `bezeichnung`) VALUES
-(1, 'Hannover, Steintor'),
-(2, 'Hannover, Hauptbahnhof'),
-(3, 'Hannover, Silas Home'),
-(4, 'tbahnhof'),
-(5, 'test'),
-(6, 'Irgendwo'),
-(7, 'Ich brauch noch eine für fix'),
-(8, 'Julian'),
-(9, 'tester'),
-(10, 'test12'),
-(11, 'test34');
+(1, 'Hannover, Hauptbahnhof'),
+(2, 'Hannover, Steintor'),
+(3, 'Hameln, Bahnhof'),
+(4, 'Münster, Hauptbahnhof'),
+(5, 'Münster, Nienberge'),
+(6, 'Münster, Häger'),
+(7, 'Hameln, HSW'),
+(8, 'Hameln, Stadtgalerie'),
+(9, 'Voldagsen, Bahnhof'),
+(10, 'Elze, Bahnhof');
 
 -- --------------------------------------------------------
 
@@ -133,21 +139,24 @@ CREATE TABLE `linienabfolge` (
 --
 
 INSERT INTO `linienabfolge` (`lid`, `bidh`, `bidr`, `vid`, `position`) VALUES
-(56, 1, 2, 92, 0),
-(57, 1, 2, 93, 1),
-(58, 1, 2, 94, 2),
-(59, 1, 2, 95, 3),
-(60, 1, 2, 96, -1),
-(61, 1, 2, 97, -2),
-(62, 1, 2, 98, 4),
-(63, 1, 2, 99, 5),
-(65, 5, 6, 104, 0),
-(66, 5, 6, 105, 1),
-(67, 5, 6, 106, 2),
-(68, 5, 6, 107, 3),
-(69, 11, 12, 108, 0),
-(70, 11, 12, 109, 1),
-(71, 11, 12, 93, 2);
+(1, 1, 2, 1, 0),
+(2, 1, 2, 2, 1),
+(3, 1, 2, 3, 2),
+(4, 1, 2, 4, 3),
+(5, 1, 2, 5, 4),
+(6, 1, 2, 6, 5),
+(7, 3, 4, 7, 0),
+(8, 3, 4, 8, 1),
+(9, 3, 4, 9, 2),
+(10, 3, 4, 10, 3),
+(11, 3, 4, 11, 4),
+(12, 3, 4, 12, 5),
+(13, 5, 6, 1, 0),
+(14, 5, 6, 13, 1),
+(15, 5, 6, 14, 2),
+(16, 5, 6, 15, 3),
+(17, 5, 6, 16, 4),
+(18, 5, 6, 17, 5);
 
 -- --------------------------------------------------------
 
@@ -191,23 +200,23 @@ CREATE TABLE `verbindung` (
 --
 
 INSERT INTO `verbindung` (`vid`, `hidS`, `hidE`, `dauer`) VALUES
-(92, 1, 2, 2),
-(93, 2, 3, 9),
-(94, 3, 4, 5),
-(95, 4, 5, 30),
-(96, 6, 1, 3),
-(97, 7, 6, 3),
-(98, 5, 8, 11),
-(99, 8, 9, 5),
-(100, 5, 2, 3),
-(101, 3, 2, 8),
-(102, 2, 1, 0),
-(104, 1, 6, 0),
-(105, 6, 2, 0),
-(106, 2, 8, 0),
-(107, 8, 5, 0),
-(108, 10, 11, 0),
-(109, 11, 2, 0);
+(1, 1, 2, 5),
+(2, 2, 3, 30),
+(3, 3, 7, 7),
+(4, 7, 8, 5),
+(5, 8, 9, 40),
+(6, 9, 10, 20),
+(7, 4, 5, 6),
+(8, 5, 6, 13),
+(9, 6, 3, 55),
+(10, 3, 1, 2),
+(11, 1, 8, 6),
+(12, 8, 7, 3),
+(13, 2, 10, 65),
+(14, 10, 9, 32),
+(15, 9, 7, 6),
+(16, 7, 5, 1),
+(17, 5, 3, 50);
 
 --
 -- Indizes der exportierten Tabellen
@@ -266,22 +275,22 @@ ALTER TABLE `verbindung`
 -- AUTO_INCREMENT für Tabelle `buslinie`
 --
 ALTER TABLE `buslinie`
-  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT für Tabelle `fahrt`
 --
 ALTER TABLE `fahrt`
-  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT für Tabelle `haltestelle`
 --
 ALTER TABLE `haltestelle`
-  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT für Tabelle `linienabfolge`
 --
 ALTER TABLE `linienabfolge`
-  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
@@ -291,7 +300,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `verbindung`
 --
 ALTER TABLE `verbindung`
-  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- Constraints der exportierten Tabellen
 --
